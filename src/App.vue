ee9a8ecf6797b2e95e36df287a5f647c76ae2374
AlessandroLembo <alessandrolembo2@gmail.com>
2023-03-15 17:36:52 +0100

    api call and print on page project name

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import AppHeader from './components/AppHeader.vue';
 // Salvo in una variabile la base APi (8000 è la porta del server su cui gira l'app del mio progetto)
 const apiBaseUrl = 'http://localhost:8000/api/';
 
+
 export default {
   name: 'App',
   components: { AppHeader },
@@ -12,28 +13,28 @@ export default {
     projects: []
   }),
 
-  method: {
+  methods: {
     fetchProjects() {
       axios.get(apiBaseUrl + 'projects').then(res => {
-        console.log(res.data);
+        this.projects = res.data;
       });
     },
+  },
 
-    created() {
-      this.fetchProjects();
-    }
+  created() {
+    this.fetchProjects();
   }
 };
 </script>
 
 <template>
   <div class="container">
-    <app-header>
-      <main>
-        <ul>
-          <li></li>
-        </ul>
-      </main>
-    </app-header>
+    <app-header></app-header>
+    <main>
+      <ul v-if="projects.length">
+        <li v-for="project in projects" :key="project.id">{{ project.name }}</li>
+      </ul>
+      <h2 v-else class="my-4">Non ci sono progetti</h2>
+    </main>
   </div>
 </template>
