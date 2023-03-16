59527220e408bdb19477ac1bad28978df468980b
AlessandroLembo <alessandrolembo2@gmail.com>
2023-03-16 18:10:05 +0100

    create component for detail projects

---
+++ src/pages/Projects/ProjectDetailPage.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    name: 'ProjectDetailPage',
+};
+</script>
+
+<template>
+    <h1>Detail</h1>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import {createRouter, createWebHistory} from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import ContactPage from '../pages/ContactPage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
+import ProjectDetailPage from '../pages/Projects/ProjectDetailPage.vue';
+
 
 
 // Creiamo il router e definiamo le rotte
@@ -17,6 +19,10 @@ const router = createRouter({
     routes: [
         { path: '/', name: 'home', component: HomePage },
         { path: '/contact', name: 'contact', component: ContactPage },
+
+        // rotta per il singolo progetto
+        { path: '/projects/:id', name: 'project-detail', component: ProjectDetailPage },
+
 
         // rotta per gestire pagine non esistenti
         {path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundPage}
