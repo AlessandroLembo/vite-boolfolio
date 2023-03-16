534aaf25da853a420477871340f165603c33d19f
AlessandroLembo <alessandrolembo2@gmail.com>
2023-03-16 17:32:42 +0100

    manage non existent page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'NotFoundPage',
+};
+</script>
+
+<template>
+    <div class="container vh-100 d-flex justify-content-center align-items-center">
+        <h2>404 | Not Found</h2>
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import {createRouter, createWebHistory} from 'vue-router';
 // Importo le pagine che voglio usare
 import HomePage from '../pages/HomePage.vue';
 import ContactPage from '../pages/ContactPage.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
 
 
 // Creiamo il router e definiamo le rotte
@@ -15,6 +16,9 @@ const router = createRouter({
     routes: [
         { path: '/', name: 'home', component: HomePage },
         { path: '/contact', name: 'contact', component: ContactPage },
+
+        // rotta per gestire pagine non esistenti
+        {path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundPage}
     ]
 });
 
