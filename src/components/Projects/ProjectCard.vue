cf7b167891a1b1781a0c89f7ecfd65741fd0deb7
AlessandroLembo <alessandrolembo2@gmail.com>
2023-03-17 15:51:01 +0100

    add computed to transform date

--- src/components/Projects/ProjectCard.vue
+++ src/components/Projects/ProjectCard.vue
@@ -1,34 +1,56 @@
 <script>
 export default {
     name: 'ProjectCard',
-    props: { project: Object, isDetail: Boolean }
+    props: { project: Object, isDetail: Boolean },
+    computed: {
+        projectDate() {
+            const date = new Date(this.project.updated_at);
+            let day = date.getDay();
+            let month = date.getMonth();
+            const year = date.getFullYear();
+            const hours = date.getHours();
+            const minutes = date.getMinutes();
+
+            if (day < 10) day = "0" + day;
+            if (month < 10) month = "0" + month;
+            if (hours < 10) hours = "0" + hours;
+            if (minutes < 10) minutes = "0" + minutes;
+
+            return `${day}/${month}/${year} - ${hours}:${minutes}`;
+
+        }
+    }
 };
 </script>
 
 <template>
-    <div class="project-card text-center">
-        <div class="ps-card d-flex flex-column justify-content-between align-items-center">
-            <div>
-                <h3>{{ project.name }}</h3>
-                <p v-if="project.description">{{ project.description }}</p>
-                <p><i>Progetto per: </i>{{ project.project_for }}</p>
-                <p v-if="project.web_platform"><i>Piattaforme: </i>{{ project.web_platform }}</p>
-                <!-- <p><i>Ultima modifica: </i>{{ projectDate }}</p>S -->
-            </div>
+    <!-- <div class="project-card text-center">
+                    <div class="ps-card d-flex flex-column justify-content-between align-items-center">
+                        <div>
+                            <h3>{{ project.name }}</h3>
+                            <p v-if="project.description">{{ project.description }}</p>
+                            <p><i>Progetto per: </i>{{ project.project_for }}</p>
+                            <p v-if="project.web_platform"><i>Piattaforme: </i>{{ project.web_platform }}</p>
+                            <p><i>Ultima modifica: </i>{{ projectDate }}</p>
+                        </div> -->
 
-            <!-- bindo il to, metto il nome della rotta e passo il paramentro che mi serve, l'id -->
+    <!-- bindo il to, metto il nome della rotta e passo il paramentro che mi serve, l'id -->
 
-            <router-link v-if="!isDetail" :to="{ name: 'project-detail', params: { id: project.id } }"
-                class="btn btn-sm btn-success">Vedi</router-link>
+    <!-- <router-link v-if="!isDetail" :to="{ name: 'project-detail', params: { id: project.id } }"
+                            class="btn btn-sm btn-success">Vedi</router-link>
+                    </div>
+                </div> -->
+    <div class="card">
+        <div class="card-header">
+            <h3>{{ project.name }}</h3>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
 </template>
 
-<style scoped lang="scss">
-.ps-card {
-    min-height: 350px;
-    padding: 1.5rem;
-    border: 1px solid lightgray;
-}
-</style>
+<style scoped lang="scss"></style>
 
--- src/components/Projects/ProjectsList.vue
+++ src/components/Projects/ProjectsList.vue
@@ -9,9 +9,8 @@ export default {
 
 <template>
     <section id="projects-list">
-        <div v-if="projects.length" class="row-projects">
-            <project-card v-for="project in projects" :key="project.id" :project="project"
-                class="col-projects"></project-card>
+        <div v-if="projects.length" class="">
+            <project-card v-for="project in projects" :key="project.id" :project="project" class=""></project-card>
 
         </div>
         <h2 v-else class="my-4">Non ci sono progetti</h2>
@@ -19,16 +18,4 @@ export default {
     </section>
 </template>
 
-<style scoped lang="scss">
-.row-projects {
-    display: flex;
-    flex-wrap: wrap;
-}
-
-.col-projects {
-    flex-basis: calc(100% / 3);
-    min-height: 350px;
-    padding: 1.5rem;
-
-}
-</style>
+<style scoped lang="scss"></style>
